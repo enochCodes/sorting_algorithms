87fe8f13e3be5b1d6f47f3a875e7de44379ed733
enochCodes <henoksamson57@gmail.com>
2024-05-01 02:17:54 +0300

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 #include <stddef.h>
-#include <stdbool.h>
+
+
 /**
  * bubble_sort - Sorts an array of integers in ascending order
  * @array: The array to be sorted
@@ -10,26 +11,19 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	bool swapped;
 
-	if (array == NULL || size < 2)
-		return;
 
-	for (i = 0; i < size - 1; i++)
+	for(i = 0; i < size; i++)
 	{
-		swapped = false;
-		for (j = 0; j < size - i - 1; j++)
+		for ( j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = true;
 				print_array(array, size);
 			}
 		}
-		if (!swapped)
-			break;
 	}
 }
